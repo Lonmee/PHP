13f9987441cff4a1b832e742dacd71880e12f962
Lonmee <Lonmee@126.com>
2017-01-13 22:40:32 +0800

    From Mac

---
+++ files/index.php
@@ -0,0 +1,14 @@
+<?php
+/**
+ * Created by PhpStorm.
+ * User: Lonmee
+ * Date: 1/13/17
+ * Time: 10:32 PM
+ */
+
+$dir    = '/tmp';
+$files1 = scandir($dir);
+$files2 = scandir($dir, 1);
+
+print_r($files1);
+print_r($files2);
