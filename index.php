3c4c297540d8f9121e55785d0c4fced46938903a
Lonmee <Lonmee@126.com>
2017-01-13 22:45:21 +0800

    From Mac

--- index.php
+++ index.php
@@ -1,6 +1,1 @@
 <?php
-require_once 'Emp.php';
-session_start();
-$_SESSION['name'] = 'lonmee';
-$_SESSION['array'] = ['name' => 'lunar', 'sex' => 'femal', 'age' => 35];
-$_SESSION['emp'] = new Emp(1, 'lonmee', 1, 'lonmee@126.com', 100);
